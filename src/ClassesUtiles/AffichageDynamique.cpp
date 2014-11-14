4a8e4c316667daf9c92ac1dafb2b80b6c5fda974
Sebastien VALLEE <E115308K@I019V7pc14.ensinfo.sciences.univ-nantes.prive>
2014-11-14 14:15:20 +0100

    aff

--- src/ClassesUtiles/AffichageDynamique.cpp
+++ src/ClassesUtiles/AffichageDynamique.cpp
@@ -1,11 +1,57 @@
-public class AffichageDynamique{
-	
-	private :
-		Donnee donnee;
-	protected :
+class AffichageDynamique{
+
+#include<stdio.h>
+#include<iostream>
+#define esc 27
+
+	public:
 		
-	public :
-		refresh
+		AffichageDynamique(){}
+		
+		void couleur(char c){/* change la couleur du texte (couleur de 30 a 47) */
+			printf("%c[%dm",esc,c);
+		}
+		
+		void bip(int n){/* fait bipper l'ordinateur n fois */
+			do{
+				printf ("%c" , 7) ; n--;
+			}while (n>0) ;
+		}
+		
+		void gras(){/* écrit en gras */
+			printf ("%c[1m",esc);
+		}
+		
+		void effacerLigne(){/* efface la ligne */
+			printf ("%c[0K",esc);
+		}
+		
+		void toutEffacer(){/* efface toute la console */
+			printf ("%c[2J%c[H" , esc , esc);
+		}
+		
+		void cursoff(){/* dés-affiche le curseur */
+			printf ("%c[?25l",esc);
+		}
+		
+		void curson(){ /* affiche le curseur */
+			printf ("%c[?25h",esc);
+		}
+		
+		void normal(){/* écrit normalement */
+			printf ("%c[0;24m",esc);
+		}  /* retour de bold , overstrike , underline */
+		
+		void poscur(int l,int c){/* positionne le curseur, ligne, colonne */
+			printf ("%c[%1d;%1dH" , esc , l , c);
+		}
+		
+		void souligner(){ /* souligne */
+			printf ("%c[4m",esc) ;
+		}
+		
+		void clignoter(){ /* fait clignoter le texte*/
+			printf ("%c[5m",esc);
+		}
 
-
-}
+};
--- src/ClassesUtiles/Interface.cpp
+++ src/ClassesUtiles/Interface.cpp
@@ -1,4 +1,26 @@
-public class interface joueur{
-	
+#include<stdio.h>
+#include<iostream>
+#include <unistd.h>
+#define esc 27
 
+#include "AffichageDynamique.cpp"
+
+using namespace std;
+
+int main(){
+	AffichageDynamique * hey = new AffichageDynamique();
+	
+	cout<<"jean louis david"<<endl;
+	
+	hey->couleur('d');
+	
+	cout<<"xD"<<endl;
+	
+	sleep(1);
+	
+	hey->normal();
+	hey->souligner();
+	cout<<"blblblblbl"<<endl;
+	
+	hey->toutEffacer();
 }

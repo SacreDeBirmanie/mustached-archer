68be2606aa3684e1185b40b476b7dfb2ea1a4bac
BerliozLeChat <neoflash93@gmail.com>
2014-12-01 19:49:50 +0100

    doc comportement

--- src/Joueurs/Comportement.cpp
+++ src/Joueurs/Comportement.cpp
@@ -1,5 +1,42 @@
+/*!
+* \file Comportement.cpp
+* \brief Fichier contenant l'implémentation de la classe Comportement
+* \author François Hallereau
+* \author Sébastien Vallée
+* \date 12.2014
+*/
+
+//--------------------------------------------------
+/*!
+* \brief Constructeur de la classe
+*/
 Comportement::Comportement(){}
 
+//--------------------------------------------------
+/*!
+* \brief Destructeur de la classe
+*/
+Comportement::~Comportement(){}
+
+//--------------------------------------------------
+/*!
+* \brief Méthode qui fait choisir un personnage au joueur
+* \param p la partie
+* \param persosDispo les personnages disponibles
+* \param j le joueur
+*/
 void Comportement::choisirPersonnage(Partie * p, vector<Personnage*> persosDispo,Joueur * j){}
+
+//--------------------------------------------------
+/*!
+* \brief Méthode qui fait choisir au joueur de tirer des cartes ou prendre des pièces 
+* \param j le joueur
+*/
 void Comportement::choisirGainTour(Joueur * j){}
+
+//--------------------------------------------------
+/*!
+* \brief Méthode qui fait choisir au joueur quel batiment construire
+* \param j le joueur
+*/
 void Comportement::choisirConstruction(Joueur * j){}
--- src/Joueurs/Comportement.hpp
+++ src/Joueurs/Comportement.hpp
@@ -1,12 +1,24 @@
+/*!
+* \file Comportement.hpp
+* \brief Fichier contenant les entêtes de la classe Comportement
+* \author François Hallereau
+* \author Sébastien Vallée
+* \date 12.2014
+*/
+
 #ifndef COMPORTEMENT_HPP
 #define COMPORTEMENT_HPP
 
+//--------------------------------------------------
+/*!
+* \class Comportement
+* \brief Classe gérant les comportements des joueurs
+*/
 class Comportement{
     public :
 		Comportement();
-
+		~Comportement();
 		virtual void jouer(Partie * p,Joueur *j) = 0;
-
 		void choisirPersonnage(Partie * p, vector<Personnage*> persosDispo,Joueur * j);
 		void choisirGainTour(Joueur * j);
 		void choisirConstruction(Joueur * j);
